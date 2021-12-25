ae66097f00b7e7c6922a4ac1eeb761ac08433a52
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-25 21:58:56 +0330

    build book and film class extends media

--- ex2.js
+++ ex2.js
@@ -4,6 +4,33 @@ class Media {
       	this.year = year;
     }
   	info(){
-    	return `${this.name}`;
+        let res = `name : ${this.name}  year : ${this.year}`
+    	return res;
+    }
+}
+
+//Book
+class Book extends Media{
+    constructor(name , year , publisher){
+        super(name , year)
+        this.publisher = publisher
+    }
+
+    info(){
+        super()
+        res += `publisher : ${this.publisher}`
+    }
+}
+
+//Film
+class Film extends Media{
+    constructor(name , year , imdb){
+        super(name , year)
+        this.imdb = imdb
+    }
+
+    info(){
+        super()
+        res += `imdb : ${this.imdb}`
     }
 }
