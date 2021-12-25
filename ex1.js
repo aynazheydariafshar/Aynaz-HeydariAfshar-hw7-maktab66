7cb78bfd06b845b22ed5ebb794d66803b849bdd5
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-25 21:27:22 +0330

    build 2 object

--- ex1.js
+++ ex1.js
@@ -1,3 +1,14 @@
 function Lesson(className , classUnit , classCapacity){
-    
+    this.className = className
+    this.classUnit = classUnit
+    this.classCapacity = classCapacity
 }
+
+const softEngineeringClass = new Lesson("softEgineering" , 3 , 50)
+const networkClass = new Lesson("network" , 3 , 30)
+
+softEngineeringClass.project = true
+networkClass.book = true
+
+console.log(JSON.stringify(softEngineeringClass))
+console.log(JSON.stringify(networkClass))
